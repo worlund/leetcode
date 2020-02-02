125cdc1465f9d4bfb3ebdd00f54cba835de1fd21
worlund <worlund@kth.se>
2020-02-02 17:22:07 +0100

    cyclic linked list

---
+++ Easy/cyclicLinkedList.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+
+        visited = set()
+
+        while head is not None:
+            visited.add(head)
+            if head.next in visited:
+                return True
+            head = head.next
+
+        return False

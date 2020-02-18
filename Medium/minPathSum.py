c51a18729f5dbc5fcabde6aff04a1d234783da2f
worlund <worlund@kth.se>
2020-02-18 23:15:46 +0100

    add strings

---
+++ Medium/minPathSum.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+
+        for i in range(1, m):
+            dp[i][0] = dp[i - 1][0] + grid[i][0]
+
+        for j in range(1, n):
+            dp[0][j] = dp[0][j - 1] + grid[0][j]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                left, above = dp[i][j - 1], dp[i - 1][j]
+                dp[i][j] = min(left + grid[i][j], above + grid[i][j])
+
+        return dp[m - 1][n - 1]
